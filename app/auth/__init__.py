05911768f9658ab33822c01e62d73433d89c4a2b
likanug <likanug.g@qq.com>
2026-01-06 15:19:37 +0800

    feat: 移除app/auth/github.py模块

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,6 +1,5 @@
 from .auth import require_permission
-from .github import exchange_code_for_token, fetch_github_user, generate_state
 from .jwt import create_access_token
 from .swagger_auth import swagger_auth
 
-__all__ = ["exchange_code_for_token", "fetch_github_user", "generate_state", "create_access_token", "require_permission", "swagger_auth"]
+__all__ = [ "create_access_token", "require_permission", "swagger_auth"]
